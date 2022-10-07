21ddba341b0c6821f4dc70ae4e4b8d7ea5de0b18
pat <pat.escalona7@gmail.com>
2022-10-07 09:34:04 -0500

    Looping through library array

--- library.js
+++ library.js
@@ -1,1 +1,44 @@
 // library.js
+
+let myLibrary = [{
+    title: "book1",
+    author: "author1",
+    pages: "pages1",
+    read: "yes"
+},
+{
+    title: "book2",
+    author: "author2",
+    pages: "pages2",
+    read: "no"
+}
+];
+
+function Book() {
+    // the constructor
+}
+
+function addBookToLibrary() {
+    // add a book
+}
+
+// write function to loop through the books and display them on a page
+function showBooks() {
+    //loop through and show books in myLibrary
+
+    const divBookContainer = document.getElementById("containerId")
+    const bookListItem = document.createElement("div")
+    const divText = document.createTextNode("new node")
+    bookListItem.appendChild(divText)
+    divBookContainer.appendChild(bookListItem)
+    myLibrary.forEach(element => {
+
+
+        console.log(element)
+
+
+    });
+
+}
+
+showBooks();
