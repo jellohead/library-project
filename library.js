b809faa7e53d546ac7deac72688316c69dc46cb1
Pat Escalona <pat.escalona7@gmail.com>
2022-10-20 14:39:12 -0500

    using localStorage instead of fetch

--- library.js
+++ library.js
@@ -25,6 +25,11 @@ const $title = document.querySelector('#title');
 const $author = document.querySelector('#author');
 const $pages = document.querySelector('#pages');
 const $read = document.querySelector('#read');
+const $form = document.querySelector('#bookForm').addEventListener('submit', (e) => {
+    e.preventDefault();
+    addBookToLibrary();
+})
+
 
 // constructor
 function Book(title, author, pages, read) {
@@ -36,31 +41,20 @@ function Book(title, author, pages, read) {
 
 function addBookToLibrary(event) {
     console.log(event)
-    // add a book
-    // create a form to enter book data
-    // call constructor by clicking a button
-    // add book to myLibrary and append to table
+    const newBook = new Book($title.title, $author.author, $pages.pages, $read.read);
+    myLibrary.push(newBook);
+    updateLocalStorage();
 }
 
-function sendData(data) {
-    console.log('Sending data')
-    const XHR = new XMLHttpRequest()
-    const urlEncodedDataPairs = []
-    for (const [name, value] of Object.entries(data)) {
-        urlEncodedDataPairs.push(`${encodeURIComponent(name)}=${encodeURIComponent(value)}`)
+function updateLocalStorage() {
+    if (localStorage.getItem('myLibrary')) {
+        myLibrary = JSON.parse(localStorage.getItem('myLibrary'));
+    } else {
+        myLibrary = DEFAULT_DATA;
     }
-
-    const urlEncodedData = urlEncodedDataPairs.join('&').replace(/%20/g, '+')
-    XHR.addEventListener('load', (event) => {
-        alert('Yeah! Data sent and response loaded')
-    })
-
-    XHR.addEventListener('error', (event) => {
-        alert('Oops! Something went wrong.')
-    })
-
-    XHR.open('POST', 't')
 }
+
+
 
 // write function to loop through the books and display them on a page
 function showBooks() {
@@ -85,39 +79,8 @@ function showBooks() {
     });
 }
 
-async function handleFormSubmit(event) {
-    event.preventDefault();
-    const form = event.currentTarget;
-    const url = form.action;
-
-    try {
-        const formData = new FormData(form);
-        const responseData = await postFormDataAsJson({ url, formData });
-        console.log({ responseData });
-    } catch (error) {
-        console.error(error);
-    }
-}
-
-async function postFormDataAsJson({ url, formData }) {
-    const plainFormData = Object.fromEntries(formData.entries());
-    const formDataJsonString = JSON.stringify(plainFormData);
-
-    const fetchOptions = {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-            "Accept": "application/json"
-        },
-        body: formDataJsonString,
-    };
-    const response = await fetch(url, fetchOptions);
-
-    if (!response.ok) {
-        const errorMessage = await response.text();
-        throw new Error(errorMessage);
-    }
-    return response.json()
+function render() {
+    checkLocalStorage();
 }
 
 
@@ -128,7 +91,7 @@ window.addEventListener('DOMContentLoaded', (event) => {
     // get form data
     console.log("set event listener for submit button")
     const form = document.querySelector('#bookForm')
-    console.log(...form);
+    console.log(form);
     form.addEventListener("submit", handleFormSubmit);
 
 
